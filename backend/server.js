e98603da18f033dece59c6cfaa6e8e15f3177b5e
Arbab Hussain <zackh41@gmail.com>
2021-01-06 20:59:43 +0530

    setup backend models, routes and middleware

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
-import products from "./data/products.js";
+import connectDB from "./config/db.js";
+import productRoutes from "./routes/productsRoutes.js";
+
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 const app = express();
-
 dotenv.config();
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("Api Start Working");
 });
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((product) => product._id === req.params.id);
-  res.json(product);
-});
+
+app.use("/api/products", productRoutes);
+app.use(notFound);
+
+app.use(errorHandler);
 
 const port = process.env.PORT || 5000;
 
