9dcedeb2cf7c5e7895c4903331212be41c35bb01
Arbab Hussain <zackh41@gmail.com>
2021-01-10 14:58:03 +0530

    cart in store and add setup complete mvc flow by adding contollor in backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HomeScreen from "./Screens/HomeScreen";
 import ProductScreen from "./Screens/ProductScreen";
+import CartScreen from "./Screens/CartScreen";
 
 import { BrowserRouter, Route } from "react-router-dom";
 
@@ -15,6 +16,7 @@ const App = () => {
         <Container>
           <Route exact path="/" component={HomeScreen} />
           <Route exact path="/product/:id" component={ProductScreen} />
+          <Route exact path="/cart/:id?" component={CartScreen} />
         </Container>
       </main>
       <Footer />
--- frontend/src/Screens/ProductScreen.jsx
+++ frontend/src/Screens/ProductScreen.jsx
@@ -7,73 +7,109 @@ import {
   Button,
   Card,
   ListGroupItem,
+  Form,
 } from "react-bootstrap";
 import Rating from "../components/Rating";
-// import products from "../products";
-import axios from "axios";
+
+import { useDispatch, useSelector } from "react-redux";
+import { listProductDetails } from "../actions/productActions";
 
 import { Link } from "react-router-dom";
-const ProductScreen = ({ match }) => {
+
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+const ProductScreen = ({ history, match }) => {
+  const [qty, setQty] = useState(1);
   // const product = products.find((product) => product._id === match.params.id);
 
-  const [product, setProduct] = useState([]);
+  const addToCart = () => {
+    history.push(`/cart/${match.params.id}?qty=${qty}`);
+  };
 
+  const dispatch = useDispatch();
   useEffect(() => {
-    const fetchProduct = async () => {
-      const { data } = await axios.get(`/api/products/${match.params.id}`);
+    dispatch(listProductDetails(match.params.id));
+  }, [dispatch, match]);
 
-      setProduct(data);
-    };
-    fetchProduct();
-  });
+  const productDetails = useSelector((state) => state.productDetails);
 
+  const { product, loading, error } = productDetails;
   return (
     <>
       <Link className="btn btn-dark my-3" to="/">
         Go Back
       </Link>
-      <Row>
-        <Col md={6}>
-          <Image src={product.image} alt={product.name} fluid />
-        </Col>
-        <Col md={3}>
-          <ListGroup variant="flush">
-            <ListGroupItem>
-              <h2>{product.name}</h2>
-            </ListGroupItem>
-            <ListGroupItem>
-              <Rating
-                value={product.rating}
-                text={`${product.numReviews} Reviews`}
-              />
-            </ListGroupItem>
-            <ListGroupItem>Price : {product.price}</ListGroupItem>
-            <ListGroupItem>Description : {product.description}</ListGroupItem>
-          </ListGroup>
-        </Col>
-        <Col md={3}>
-          <Card>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Row>
+          <Col md={6}>
+            <Image src={product.image} alt={product.name} fluid />
+          </Col>
+          <Col md={3}>
             <ListGroup variant="flush">
               <ListGroupItem>
-                <strong>Price : {product.price}</strong>
+                <h2>{product.name}</h2>
               </ListGroupItem>
               <ListGroupItem>
-                Status :{" "}
-                {product.countInStock > 0 ? "In Stock" : "Out Of Stock"}
+                <Rating
+                  value={product.rating}
+                  text={`${product.numReviews} Reviews`}
+                />
               </ListGroupItem>
-              <ListGroupItem>
-                <Button
-                  className="btn btn-block btn-dark"
-                  type="button"
-                  disabled={product.countInStock === 0}
-                >
-                  Add To Cart
-                </Button>
-              </ListGroupItem>
+              <ListGroupItem>Price : {product.price}</ListGroupItem>
+              <ListGroupItem>Description : {product.description}</ListGroupItem>
             </ListGroup>
-          </Card>
-        </Col>
-      </Row>
+          </Col>
+          <Col md={3}>
+            <Card>
+              <ListGroup variant="flush">
+                <ListGroupItem>
+                  <strong>Price : {product.price}</strong>
+                </ListGroupItem>
+                <ListGroupItem>
+                  Status :{" "}
+                  {product.countInStock > 0 ? "In Stock" : "Out Of Stock"}
+                </ListGroupItem>
+
+                {product.countInStock > 0 && (
+                  <ListGroupItem>
+                    <Row>
+                      <Col>Qty:</Col>
+                      <Col>
+                        <Form.Control
+                          as="select"
+                          value={qty}
+                          onChange={(e) => setQty(e.target.value)}
+                        >
+                          {[...Array(product.countInStock).keys()].map((x) => (
+                            <option key={x + 1} value={x + 1}>
+                              {x + 1}
+                            </option>
+                          ))}
+                        </Form.Control>
+                      </Col>
+                    </Row>
+                  </ListGroupItem>
+                )}
+
+                <ListGroupItem>
+                  <Button
+                    className="btn btn-block btn-dark"
+                    type="button"
+                    disabled={product.countInStock === 0}
+                    onClick={addToCart}
+                  >
+                    Add To Cart
+                  </Button>
+                </ListGroupItem>
+              </ListGroup>
+            </Card>
+          </Col>
+        </Row>
+      )}
     </>
   );
 };
