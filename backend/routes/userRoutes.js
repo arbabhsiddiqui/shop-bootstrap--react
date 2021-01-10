76445ee4e68fc0780242d460d04c2c7339d525db
Arbab Hussain <zackh41@gmail.com>
2021-01-10 16:36:40 +0530

    error while getting user from db

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,8 @@
 import express from "express";
 
 const router = express.Router();
+import { userAuth } from "../controllers/userController.js";
+
+router.post("/login", userAuth);
 
 export default router;
--- backend/seeder.js
+++ backend/seeder.js
@@ -3,9 +3,9 @@ import dotenv from "dotenv";
 
 import users from "./data/users.js";
 import products from "./data/products.js";
-import User from "./model/userModel.js";
-import Product from "./model/productModel.js";
-import Order from "./model/orderModel.js";
+import User from "./models/userModel.js";
+import Product from "./models/productModel.js";
+import Order from "./models/orderModel.js";
 import connectDB from "./config/db.js";
 
 dotenv.config();
