a615bfeb534ee56ab37a4ebbc0cc070b1ff49f08
Arbab Hussain <zackh41@gmail.com>
2021-01-12 21:47:25 +0530

    auth and token system in backend complete

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,17 +1,18 @@
 import asyncHandler from "express-async-handler";
-import User from "../models/productModel.js";
+import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
 
 const userAuth = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
-  const user = await User.findOne({ email });
+  const user = await User.findOne({ email: email });
   if (user && (await user.matchPassword(password))) {
     res.json({
       _id: user._id,
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null,
+      token: generateToken(user._id),
     });
   } else {
     res.status(404);
@@ -19,4 +20,52 @@ const userAuth = asyncHandler(async (req, res) => {
   }
 });
 
-export { userAuth };
+//
+
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Error While Registering");
+  }
+});
+
+export { userAuth, getUserProfile, registerUser };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,16 @@
 import express from "express";
 
 const router = express.Router();
-import { userAuth } from "../controllers/userController.js";
+import {
+  userAuth,
+  getUserProfile,
+  registerUser,
+} from "../controllers/userController.js";
+import protect from "../middleware/authMiddleware.js";
 
+router.route("/").post(registerUser);
 router.post("/login", userAuth);
+// router.get("/profile", getUserProfile);
+router.route("/profile").get(protect, getUserProfile);
 
 export default router;
